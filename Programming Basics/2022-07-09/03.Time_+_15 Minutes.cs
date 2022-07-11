54ce8c803e6c2b173f64655c3abf57d6d080d620
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-11 17:29:54 +0300

    Create 03.Time_+_15 Minutes.cs

---
+++ Programming Basics/2022-07-09/03.Time_+_15 Minutes.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            minutes += 15;
+            if (minutes >= 60)
+            {
+                minutes -= 60;
+                hours += 1;
+            }
+            if (hours >= 24)
+            {
+                hours = 0;
+            }
+            if(minutes < 10)
+            {
+                Console.WriteLine($"{hours}:0{minutes}");
+            }
+            if(minutes >= 10)
+            {
+                Console.WriteLine($"{hours}:{minutes}");
+            }
+             
+        }
+    }
+}
