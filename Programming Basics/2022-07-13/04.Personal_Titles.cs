0077fc4e5f17ce4629dbf877ac07fa23e657b634
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-14 13:31:17 +0300

    Create 04.Personal_Titles.cs

---
+++ Programming Basics/2022-07-13/04.Personal_Titles.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+            
+            if(gender == "m")
+            {
+                if(age < 16)
+                {
+                    Console.WriteLine("Master");
+                }
+                else
+                {
+                    Console.WriteLine("Mr.");
+                }
+            }
+            else if(gender == "f")
+            {
+                if(age < 16)
+                {
+                    Console.WriteLine("Miss");
+                }
+                else
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+        }
+    }
+}
+
