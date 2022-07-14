44b4bce06efadcba78f144fcf8ca4ef80cfd0781
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-14 15:32:45 +0300

    Create 09.Fruit_or_Vegetable.cs

---
+++ Programming Basics/2022-07-13/09.Fruit_or_Vegetable.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string nameOfProduct = Console.ReadLine();
+
+            switch (nameOfProduct)
+            {
+                case "banana":
+                case "apple":
+                case "kiwi":
+                case "cherry":
+                case "lemon":
+                case "grapes":
+                    Console.WriteLine("fruit");
+                    break;
+                case "tomato":
+                case "cucumber":
+                case "pepper":
+                case "carrot":
+                    Console.WriteLine("vegetable");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
+
