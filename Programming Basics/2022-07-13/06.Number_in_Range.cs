8b20ed34c7285e986fd5a01be818d10898d56d5f
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-14 14:04:08 +0300

    Create 06.Number_in_Range.cs

---
+++ Programming Basics/2022-07-13/06.Number_in_Range.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number != 0 && number >= -100 && number <= 100)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
+
