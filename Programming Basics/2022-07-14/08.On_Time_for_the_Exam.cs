29162c7b91b9d30f35bfe9006e0e447565840ca9
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-15 23:20:36 +0300

    Create 08.On_Time_for_the_Exam.cs

---
+++ Programming Basics/2022-07-14/08.On_Time_for_the_Exam.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	Първият ред съдържа час на изпита – цяло число от 0 до 23.
+            //•	Вторият ред съдържа минута на изпита – цяло число от 0 до 59.
+            //•	Третият ред съдържа час на пристигане – цяло число от 0 до 23.
+            //•	Четвъртият ред съдържа минута на пристигане – цяло число от 0 до 59.
+            int hour = int.Parse(Console.ReadLine());
+            int minute = int.Parse(Console.ReadLine());
+            int hourOfArrival = int.Parse(Console.ReadLine());
+            int minuteOfArrival = int.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
+
