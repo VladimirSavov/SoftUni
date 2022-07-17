7a1283eae7040512588696d0c51b4b752a61e285
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-17 12:33:45 +0300

    Create 01.Numbers_from_1_to_100.cs

---
+++ Programming Basics/01.Numbers_from_1_to_100.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
