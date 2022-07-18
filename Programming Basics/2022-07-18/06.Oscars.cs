ec023686d29bfdd4f4c6780d9e3e2752b452703f
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-18 20:15:09 +0300

    Create 06.Oscars.cs

---
+++ Programming Basics/2022-07-18/06.Oscars.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Softu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string actorName = Console.ReadLine();
+            double points = double.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            string nameOfAppraiser = "";
+            double pointsFromTheEvaluator = 0.0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                nameOfAppraiser = Console.ReadLine();
+                pointsFromTheEvaluator = double.Parse(Console.ReadLine());
+
+                points += nameOfAppraiser.Length * pointsFromTheEvaluator / 2;
+
+                if (points >= 1250.5)
+                {
+                    Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {points:F1}!");
+                    break;
+                }
+            }
+            if (points < 1250.5)
+            {
+                Console.WriteLine($"Sorry, {actorName} you need {1250.5 - points:F1} more!");
+            }
+        }
+    }
+}
+
