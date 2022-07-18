348d516d9e8a1fd3594585993d02e5c96a855782
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-18 11:55:32 +0300

    Create 10.Odd_Even_Sum.cs

---
+++ Programming Basics/2022-07-17/10.Odd_Even_Sum.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (i % 2 != 0)
+                {
+                    sum1 += input;
+                }
+                else
+                {
+                    sum2 += input;
+                }
+            }
+
+            if (sum1 == sum2)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sum1}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sum1 - sum2)}");
+            }
+
+        }
+    }
+}
+
