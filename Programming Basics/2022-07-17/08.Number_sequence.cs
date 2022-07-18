908401543a3cc6f51e18ca0953c32d2704682e19
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-18 11:36:29 +0300

    Create 08.Number_sequence.cs

---
+++ Programming Basics/2022-07-17/08.Number_sequence.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (input < min)
+                {
+                    min = input;
+                }
+                if (input > max)
+                {
+                    max = input;
+                }
+            }
+            Console.WriteLine($"Max number: {max}");
+            Console.WriteLine($"Min number: {min}");
+        }
+    }
+}
+
