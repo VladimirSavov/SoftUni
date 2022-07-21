f39a91ca6af3d60f16cdc5c6dbbe02ac6be88eec
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-21 17:08:43 +0300

    Create 06.Max_Number.cs

---
+++ Programming Basics/2022-07-20/06.Max_Number.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int max = int.MinValue;
+            while (input != "Stop")
+            {
+                int number = int.Parse(input);
+                if(number > max)
+                {
+                    max = number;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(max);
+        }
+          
+    }
+}
+
