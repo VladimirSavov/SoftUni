369b98f0fda7d9a47ef942f3c684a599f234306c
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-25 12:34:27 +0300

    Create 05.Average_Number.cs

---
+++ Programming Basics/2022-07-24/05.Average_Number.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double total = 0.0;
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                total += number;  
+            }
+            double average = (double)total / n;
+            Console.WriteLine("{0:F2}" , average);
+        }
+
+    }
+}
+
