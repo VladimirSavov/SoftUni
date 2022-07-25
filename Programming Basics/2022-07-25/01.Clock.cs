f8b1bd374bd43ccc8a6d71c88d425042e44d8776
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-25 13:31:20 +0300

    Create 01.Clock.cs

---
+++ Programming Basics/2022-07-25/01.Clock.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hour = 0; hour < 24; hour++)
+            {
+                for (int minute = 0; minute < 60; minute++)
+                {
+                    Console.WriteLine($"{hour}:{minute}");
+                }
+            }
+        }
+
+    }
+}
+
