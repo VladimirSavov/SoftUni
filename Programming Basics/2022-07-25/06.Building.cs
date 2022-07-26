8e848cac297623a8f39de4d1cf1bde372bfc1f76
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-26 10:39:47 +0300

    Create 06.Building.cs

---
+++ Programming Basics/2022-07-25/06.Building.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfFloors = int.Parse(Console.ReadLine());
+            int numberOfRoom = int.Parse(Console.ReadLine());
+            for (int floor = numberOfFloors; floor >= 1; floor--)
+            {
+                for (int room = 0; room < numberOfRoom; room++)
+                {
+                    if(floor == numberOfFloors)
+                    {
+                        Console.Write($"L{floor}{room} ");
+                        continue;
+                    }
+                    if (floor % 2 == 0)
+                    {
+                        Console.Write($"O{floor}{room} ");
+                    }
+                    else
+                    {
+                        Console.Write($"A{floor}{room} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+        }
+
+    }
+}
+
