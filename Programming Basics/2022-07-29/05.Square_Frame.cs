d3e2f152a23580e1439c2450dd4431f0f587173e
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-29 10:05:18 +0300

    Create 05.Square_Frame.cs

---
+++ Programming Basics/2022-07-29/05.Square_Frame.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i == 0 && j == 0) || (i == n - 1 && j == n - 1) || (i == 0 && j == n - 1) || (i == n - 1 && j == 0))
+                    {
+                        Console.Write("+ ");
+                    }
+
+                    else if ((j == 0 && i != 0) || (j == n - 1 && i != n - 1))
+                    {
+                        Console.Write("| ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
