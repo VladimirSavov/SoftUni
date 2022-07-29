1c28bd814959cd8155069ca5ebe5f103a7babe7d
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-29 09:44:43 +0300

    Create 01.Rectangle_of_10_x_10_Stars.cs

---
+++ Programming Basics/2022-07-29/01.Rectangle_of_10_x_10_Stars.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("**********");
+            }
+        }
+    }
+}
