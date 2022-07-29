6c1cd4a8ddbaded358281aed8137816bbff2c788
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-29 07:04:20 +0300

    Create 11.HappyCat_Parking.cs

---
+++ Programming Basics/2022-07-28/11.HappyCat_Parking.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double days = double.Parse(Console.ReadLine());
+            double hours = double.Parse(Console.ReadLine());
+            double sumForDay = 0;
+            double totalSum = 0;
+            for (int i = 1; i <= days; i++)
+            {
+                sumForDay = 0;
+                for (int k = 1; k <= hours; k++)
+                {
+                    if(i % 2 == 0 && k % 2 != 0)
+                    {
+                        sumForDay += 2.50;
+                    }
+                    else if (i % 2 != 0 && k % 2 == 0)
+                    {
+                        sumForDay += 1.25;
+                    }
+                    else
+                    {
+                        sumForDay += 1;
+                    }
+                }
+                totalSum += sumForDay;
+                Console.WriteLine($"Day: {i} - {sumForDay:F2} leva");
+            }
+            Console.WriteLine($"Total: {totalSum:F2} leva");
+        }
+    }
+}
