b9e51d55c48bafcfced23156c2e9b463f31fcebf
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-29 10:20:32 +0300

    Create 08.Sunglasses.cs

---
+++ Programming Basics/2022-07-29/08.Sunglasses.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write(new string('*', 2 * n));
+            Console.Write(new string(' ', n));
+            Console.Write(new string('*', 2 * n));
+            Console.WriteLine();
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write("*");
+                Console.Write(new string('/', 2 * n - 2));
+                Console.Write("*");
+                if (i == (n - 1) / 2 - 1)
+                {
+                    Console.Write(new string('|', n));
+                }
+                else
+                {
+                    Console.Write(new string(' ', n));
+                }
+                Console.Write("*");
+                Console.Write(new string('/', 2 * n - 2));
+                Console.Write("*");
+
+                Console.WriteLine();
+
+            }
+            Console.Write(new string('*', 2 * n));
+            Console.Write(new string(' ', n));
+            Console.Write(new string('*', 2 * n));
+        }
+    }
+}
