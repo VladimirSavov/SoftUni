7d0ece3cf2b0f65ac8b19291d4daac924a76bdd1
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-07-29 09:47:55 +0300

    Create 02.Rectangle_of_N_x_N_Stars.cs

---
+++ Programming Basics/2022-07-29/02.Rectangle_of_N_x_N_Stars.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
