0b8db7b556d7d4b6a3ce22732a0914f0b3f313da
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-02 13:25:18 +0300

    Create 01.Hello_SoftUni.cs

---
+++ Programming Basics/2022-07-01/01.Hello_SoftUni.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }        
+    }
+}
