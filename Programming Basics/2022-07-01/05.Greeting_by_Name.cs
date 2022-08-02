0a268270c250f51d1703f468c0a2fff7e3a508d0
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-02 13:31:39 +0300

    Create 05.Greeting_by_Name.cs

---
+++ Programming Basics/2022-07-01/05.Greeting_by_Name.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");    
+        }
+    } 
+}
