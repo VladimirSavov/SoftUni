38881777e901f0ceb61385b007b2026308ea4d09
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-03 09:12:45 +0300

    Create 01.USD_to_BGN.cs

---
+++ Programming Basics/2022-07-03/01.USD_to_BGN.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * 1.79549;
+            
+            Console.WriteLine(bgn);
+        }
+    } 
+}
