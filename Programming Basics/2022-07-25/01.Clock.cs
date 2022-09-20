e7e686a72c3b7160f9fde17f28daa827e38185e0
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-09-20 21:15:51 +0300

    Create 01.Clock.cs

---
+++ Programming Basics/2022-07-25/01.Clock.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <=23; i++)
+            {
+                for (int k = 0; k <= 59; k++)
+                {
+                    Console.WriteLine($"{i}:{k}");
+                }
+            }
+        }
+    }
+}
